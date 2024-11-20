bc7bd14147dcfbeaa4e1126c21c022e53dd2b000
wbridgri <will.bridges@comcast.net>
2024-11-20 14:47:25 -0600

    finishing may need to crop photos

--- js/app.js
+++ js/app.js
@@ -23,7 +23,7 @@ const menuItems = [
         type: 'appetizers',
         item: 'salmon dip',
         desc: 'fresh salmon spread on toast',
-        imgUrl: 'https://thumbs.dreamstime.com/z/smoked-salmon-dip-selective-focus-84576595.jpg?ct=jpeg',
+        imgUrl: 'salmonDip.jpg',
         price: 10.99,
         qty: 0
     },
@@ -32,7 +32,7 @@ const menuItems = [
         type: 'appetizers',
         item: 'egg rolls',
         desc: 'vietnamese style, vegetarian',
-        imgUrl: '',
+        imgUrl: 'eggrolls.jpg',
         price: 7.99,
         qty: 0
     },
@@ -41,7 +41,7 @@ const menuItems = [
         type: 'appetizers',
         item: 'fried pickles',
         desc: 'pickled spears, hand beer-battered and made fresh to order',
-        imgUrl: '',
+        imgUrl: 'friedPickles.jpg',
         price: 7.99,
         qty: 0
     },
@@ -50,7 +50,7 @@ const menuItems = [
         type: 'appetizers',
         item: 'foie gras',
         desc: 'made by force feeding a goose until it die.....YUMMY!',
-        imgUrl: '',
+        imgUrl: 'foieGras.jpg',
         price: 20.99,
         qty: 0
     },
@@ -59,7 +59,7 @@ const menuItems = [
         type: 'entrees',
         item: 'hamburger',
         desc: '4 ounce grassfed Wagyu patty, cooked to order',
-        imgUrl: '',
+        imgUrl: 'hamburger.jpg',
         price: 15.99,
         qty: 0
     },
@@ -68,7 +68,7 @@ const menuItems = [
         type: 'entrees',
         item: 'red beans and rice',
         desc: 'made with andouille and pickled pig\'s feet',
-        imgUrl: '',
+        imgUrl: 'redBeansAndRice.jpg',
         price: 14.99,
         qty: 0
     },
@@ -77,7 +77,7 @@ const menuItems = [
         type: 'entrees',
         item: 'Steak Frites',
         desc: 'grassfed beef, dry aged to perfection, served with shoestrings fries because TikTok',
-        imgUrl: '',
+        imgUrl: 'steakFrites.jpg',
         price: 25.99,
         qty: 0
     },
@@ -86,7 +86,7 @@ const menuItems = [
         type: 'entrees',
         item: 'Rassolnik',
         desc: 'East European pickle soup, made with dill and barley, perfect for hangovers!',
-        imgUrl: '',
+        imgUrl: 'rassolnik.jpg',
         price: 13.99,
         qty: 0
     },
@@ -95,7 +95,7 @@ const menuItems = [
         type: 'drinks',
         item: 'beer',
         desc: 'domestic brews. why would you order this, you clown',
-        imgUrl: '',
+        imgUrl: 'beer.jpg',
         price: 5.00,
         qty: 0
     },
@@ -104,7 +104,7 @@ const menuItems = [
         type: 'drinks',
         item: 'whiskey',
         desc: 'my grandpappy makes it.  Hit\'s real good',
-        imgUrl: '',
+        imgUrl: 'whiskey.jpg',
         price: 10.00,
         qty: 0
     },
@@ -113,7 +113,7 @@ const menuItems = [
         type: 'drinks',
         item: 'water',
         desc: 'straight from the world class purifcation systems of Hinds County',
-        imgUrl: '',
+        imgUrl: 'water.jpg',
         price: 0.00,
         qty: 0
     },
@@ -122,7 +122,7 @@ const menuItems = [
         type: 'drinks',
         item: 'sweet tea',
         desc: 'we only have sweet, sorry if you\'re diabetic.',
-        imgUrl: '',
+        imgUrl: 'sweetTea.jpg',
         price: 2.99,
         qty: 0
     },
@@ -131,7 +131,7 @@ const menuItems = [
         type: 'desserts',
         item: 'cheesecake',
         desc: 'New York Style, frozen and dethawed in the microwave',
-        imgUrl: '',
+        imgUrl: 'cheesecake.jpg',
         price: 9.99,
         qty: 0
     },
@@ -140,7 +140,7 @@ const menuItems = [
         type: 'desserts',
         item: 'chocolate cake',
         desc: 'we outsource our chocalate cakes from McDonalds',
-        imgUrl: '',
+        imgUrl: 'chocoCake.jpg',
         price: 7.99,
         qty: 0
     },
@@ -149,7 +149,7 @@ const menuItems = [
         type: 'desserts',
         item: 'ice cream',
         desc: 'World class cream... World class ice.... How can one resist?',
-        imgUrl: '',
+        imgUrl: 'iceCream.jpg',
         price: 8.99,
         qty: 0
     },
@@ -158,7 +158,7 @@ const menuItems = [
         type: 'desserts',
         item: 'carrot cake',
         desc: 'This menu item is to show that our business cares about people who\'ve been diagnosed with sociopathy.  It gets better!',
-        imgUrl: '',
+        imgUrl: 'satanCake.jpg',
         price: 99.99,
         qty: 0
     },
@@ -167,7 +167,7 @@ const menuItems = [
         type: 'sides',
         item: 'mac and cheese',
         desc: 'made with CAVE AGED CHEDDAR',
-        imgUrl: '',
+        imgUrl: 'macAndCheese.jpg',
         price: 6.99,
         qty: 0
     },
@@ -176,7 +176,7 @@ const menuItems = [
         type: 'sides',
         item: 'rice',
         desc: 'Just white rice',
-        imgUrl: '',
+        imgUrl: 'rice.jpg',
         price: 2.99,
         qty: 0
     },
@@ -185,7 +185,7 @@ const menuItems = [
         type: 'sides',
         item: 'french fries',
         desc: 'Shoe String french fries.  They\'re shoes string because of a video I saw on TikTok',
-        imgUrl: '',
+        imgUrl: 'fries.jpg',
         price: 5.99,
         qty: 0
     },
@@ -194,7 +194,7 @@ const menuItems = [
         type: 'sides',
         item: 'gravy',
         desc: 'just a pot of gravy!',
-        imgUrl: '',
+        imgUrl: 'gravy.jpg',
         price: 3.99,
         qty: 0
     }
@@ -204,7 +204,29 @@ const menuItems = [
 
 confirmBtn.addEventListener('click', (e)=> {
     e.preventDefault()
+    getTotal()
 })
+
+//getTotal()
+const getTotal = () => {
+    const subtotal = parseFloat(cardSubtotal.innerText)
+    const tipAmt = parseFloat(document.getElementById('tipAmt').value)
+    const otherAmt = parseFloat(document.getElementById('otherAmt').value)
+    const yourTip = document.getElementById('yourTip')
+    const theSubtotal = document.getElementById('theSubtotal')
+    const taxDisplay = document.getElementById('tax')
+
+    let taxTotal = subtotal * tax
+
+    let receiptTip = isNaN(tipAmt) ? otherAmt : (subtotal * tipAmt)  //ternary operator can be stored to a value whereas if statements cannot be
+
+    let total = isNaN(tipAmt) ? subtotal + otherAmt + taxTotal : receiptTip + subtotal + taxTotal
+
+    theSubtotal.innerText = subtotal
+    taxDisplay.innerText = taxTotal.toFixed(2)
+    yourTip.innerText = receiptTip.toFixed(2)
+    totalDisplay.innerText = total.toFixed(2)
+}
 
 //load the menu items
 
@@ -242,14 +264,178 @@ menuItems.forEach(item => {
     column.classList.add('col-sm-3')
 
     const card = document.createElement('div')
-    card.classList.add('card')
+    card.classList.add('card', 'h-100')
     card.innerHTML = `
         <img src="images/${item.imgUrl}" alt="${item.desc}" class="img-fluid menu-image card-image-top" />
         <div class="card-body">
             <h4 class="card-title text-capitalize item-item">${item.item}</h4>
             <p class= "card-text text-uppercase item-desc">${item.desc}</p>
-        </div`
+        </div>
+        <footer class="card-footer">
+            <p class="card-text item-price">$${item.price}</p>
+            <div class="buttons-div d-flex justify-content-around">
+                <button
+                    class="btn btn-danger cart-btn text-capitalize"
+                    id="Btn${item.id}"
+                    data-id="${item.id}"
+                    data-price="${item.price}"
+                    data-qty="${item.qty}"
+                    data-item="${item.item}"
+                    >add to cart</button>
+                <div class="qty-div">
+                    <button
+                        class="btn btn-primary btn-subtract"
+                        id="btnSubtract${item.id}"
+                        data-id="${item.id}"
+                        data-qty="${item.qty}"
+                    > - </button> 
+                    <span class="quantity" id="quantity${item.id}">${item.qty}</span>
+                    <button
+                        class="btn btn-primary btn-add"
+                        id="btnAdd${item.id}"
+                        data-id="${item.id}"
+                        data-qty="${item.qty}"
+                    > + </button> 
+                </div>
+            </div>
+        </footer>`
+            column.appendChild(card)
+
+            //switch (menuItems.type)
+            switch(item.type) {
+                case 'appetizers': 
+                    appRow.appendChild(column)
+                    break
+                case 'entrees': 
+                    entreesRow.appendChild(column)
+                    break
+                case 'drinks': 
+                    drinksRow.appendChild(column)
+                    break
+                case 'desserts': 
+                    dessertsRow.appendChild(column)
+                    break
+                case 'sides': 
+                    sidesRow.appendChild(column)
+                    break
+                default:
+                    console.log('Error')
+                    break;
+                    
+            }
 })
+
+//cart buttons
+const cartButtons = document.querySelectorAll('.cart-btn')
+
+cartButtons.forEach(button => {
+    const price = parseFloat(button.getAttribute('data-price'))
+    const item = button.getAttribute('data-item')
+    const id = parseFloat(button.getAttribute('data-id'))
+
+    button.addEventListener('click', ()=> {
+        let qty
+        for(let i = 0; i < menuItems.length; i++) {
+            menuItems[i].id === id ? qty = menuItems[i].qty : null
+        }
+        if(button.getAttribute('data-qty') > 0) {
+            addItems(price, qty, item, id)
+        }
+    })
+})
+
+//additems()
+
+let receiptArray = []
+
+const addItems = (price, qty, item, id) => {
+    let itemObj = {
+        id: id, //you can actually just write id, item, qty, price
+        item: item,
+        qty: qty,
+        price: price,
+        itemTotal: qty * price
+    }
+
+    receiptArray = [...receiptArray, itemObj]
+
+    //makeReceipt
+    makeReceipt(itemObj, receipt)
+
+    subtotal += itemObj.itemTotal
+    console.log(subtotal.toFixed(2))
+    cardSubtotal.innerText = subtotal.toFixed(2)
+}
+
+//makeReceipt()
+const makeReceipt = (obj, el) => {
+    const tableRow = document.createElement('tr')
+    tableRow.classList.add('receipt-item')
+
+    const receiptChoice = document.createElement('td')
+    receiptChoice.classList.add('receipt-choice', 'text-center')
+    receiptChoice.innerText = obj.item
+
+    const receiptQty = document.createElement('td')
+    receiptQty.classList.add('receipt-qty', 'text-center')
+    receiptQty.innerText = obj.qty
+    receiptQty.setAttribute('id', `qty${obj.id}`)
+
+    const receiptPrice = document.createElement('td')
+    receiptPrice.classList.add('receipt-price', 'text-center')
+    receiptPrice.innerText = obj.price
+
+    const itemSubtotal = document.createElement('td')
+    itemSubtotal.classList.add('item-subtotal', 'text-center')
+    itemSubtotal.innerText = obj.itemTotal.toFixed(2)
+    itemSubtotal.setAttribute('id', `subTotal${obj.id}`)
+
+    tableRow.appendChild(receiptChoice)
+    tableRow.appendChild(receiptQty)
+    tableRow.appendChild(receiptPrice)
+    tableRow.appendChild(itemSubtotal)
+
+    el.appendChild(tableRow)
+
+}
+
+const btnSubtract = document.querySelectorAll('.btn-subtract')
+const btnAdd = document.querySelectorAll('.btn-add')
+
+btnSubtract.forEach(button => {
+    button.addEventListener('click', () => {
+        const btnId = parseFloat(button.getAttribute('data-id'))
+        const spanQty = document.getElementById(`quantity${btnId}`)
+
+        for(let i = 0; i < menuItems.length; i++) {
+            if(menuItems[i].id === btnId && menuItems[i].qty > 0) {
+                menuItems[i].qty -= 1
+                spanQty.innerText = menuItems[i].qty
+            }
+        }
+    })
+})
+
+btnAdd.forEach(button => {
+    button.addEventListener('click', () => {
+        const btnId = parseFloat(button.getAttribute('data-id'))
+        const spanQty = document.getElementById(`quantity${btnId}`)
+
+        for(let i = 0; i < menuItems.length; i++) {
+            if(menuItems[i].id === btnId && menuItems[i].qty < 20 && cartButtons[i].dataset.id == btnId) {
+                menuItems[i].qty+=1
+                cartButtons[i].setAttribute('data-qty', menuItems[i].qty)
+                spanQty.innerText = menuItems[i].qty
+            }
+        }
+    })
+})
+
+
+
+
+
+
 
 
 
